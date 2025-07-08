e266a43f8dc264171b978bc471c997583ecc64a6
Abdullah <l201326@lhr.nu.edu.pk>
2025-07-08 20:23:20 +0500

    try to run extension

---
+++ vite-popup/vite.config.ts
@@ -0,0 +1,17 @@
+// vite-popup/vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: '../extension',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        popup: path.resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+})
